18fa4e80b66b016ac6ae9a7abcf43db807648491
Valentin Hăloiu <vially.ichb@gmail.com>
2015-11-05 23:16:59 +0200

    Add negroni middleware

---
+++ negronibump/middleware.go
@@ -0,0 +1,32 @@
+package negronibump
+
+import (
+	"github.com/codegangsta/negroni"
+	"github.com/dustin/go-humanize"
+	"github.com/unrolled/render"
+	"github.com/etcinit/speedbump"
+	"gopkg.in/redis.v2"
+	"net"
+	"net/http"
+	"time"
+)
+
+func RateLimit(client *redis.Client, hasher speedbump.RateHasher, max int64) negroni.HandlerFunc {
+	limiter := speedbump.NewLimiter(client, hasher, max)
+	rnd := render.New()
+
+	return func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+		ok, err := limiter.Attempt(ip)
+		if err != nil {
+			panic(err)
+		}
+
+		if !ok {
+			nextTime := time.Now().Add(hasher.Duration())
+			rnd.JSON(rw, 429, map[string]string{"error": "Rate limit exceeded. Try again in " + humanize.Time(nextTime)})
+		} else {
+			next(rw, r)
+		}
+	}
+}
